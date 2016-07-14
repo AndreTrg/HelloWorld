a7c64e69506eb90e84e3f261fc322e98a240dcb0
AndreTrg <andre@pasiphae.eu>
2016-07-14 18:57:23 +0300

    Python Comments
    Make abjustments in comments

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-# The following line wait for user to input something before exit the program
+# With the following command the program wait for a user input to continue
 ok = raw_input('Please press enter to run the program...')
 print "Hello world!"
-# The following line wait for user to input something before exit the program
+# With the following command the program wait for a user input before exit
 ok = raw_input('Press enter to exit...')
 exit()
