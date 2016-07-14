93f40dcf4db584853a123bca06222b42a9a59b49
AndreTrg <andre@pasiphae.eu>
2016-07-14 17:44:31 +0300

    Changed the message
    I changed the message that prompts the user to press enter, in order to
    start the program running...

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # The following line wait for user to input something before exit the program
-ok = raw_input('Press enter to run program...')
+ok = raw_input('Please press enter to run the program...')
 print "Hello world!"
 # The following line wait for user to input something before exit the program
 ok = raw_input('Press enter to exit...')
