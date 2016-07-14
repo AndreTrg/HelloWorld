1216ee3274e01322b20d13a14548e17261199d05
AndreTrg <andre@pasiphae.eu>
2016-07-14 17:34:52 +0300

    Add some comments
    Added some comments about the reason i use raw_input() function.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+# The following line wait for user to input something before exit the program
 ok = raw_input('Press enter to run program...')
-print "hello world"
+print "Hello world!"
+# The following line wait for user to input something before exit the program
 ok = raw_input('Press enter to exit...')
 exit()
