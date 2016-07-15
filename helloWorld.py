b5932b7826e5ab68b374919ff415ba12d48136e1
AndreTrg <andre@pasiphae.eu>
2016-07-15 12:37:21 +0300

    remove lines of code
    Remove some lines of code. I Keeped only one input before exit()

--- helloWorld.py
+++ helloWorld.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
-# Last updated in 2016.07.14 @ 23:05
-# Using the following command, the program wait for a user input to continue
-ok = raw_input('Please press enter to run the program...')
+# Last updated in 2016.07.15 @ 12:35
 print "Hello world!"
-# Using the following command, the programm wait for a user input before exit
 ok = raw_input('Press enter to exit...')
 exit()
